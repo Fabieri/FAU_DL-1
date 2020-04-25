a20fb11d79b0ea3044b4fd27a36c4e631e33d82f
EinGartenzwerg <maximilian.harl@gmail.com>
2020-04-25 23:08:25 +0200

    pattern structure

---
+++ ex01/src_to_implement/main.py
@@ -0,0 +1,4 @@
+from ex01.src_to_implement.pattern import Checker
+from ex01.src_to_implement.pattern import Circle
+
+# do stuff!
---
+++ ex01/src_to_implement/pattern.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Checker:
+    def __init__(self):
+        self.output = None
+        pass
+
+    def draw(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Circle:
+    def __init__(self):
+        self.output = None
+        pass
+
+    def draw(self):
+        pass
+
+    def show(self):
+        pass
