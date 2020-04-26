b8611c64a8916aa02dbbc96428c3d8e04c9b8327
EinGartenzwerg <maximilian.harl@gmail.com>
2020-04-26 13:41:33 +0200

    Spectrum

--- ex01/src_to_implement/main.py
+++ ex01/src_to_implement/main.py
@@ -1,13 +1,18 @@
 from ex01.src_to_implement.pattern import Checker
 from ex01.src_to_implement.pattern import Circle
+from ex01.src_to_implement.pattern import Spectrum
 
 # do stuff!
 
 # checker = Checker(12, 2)
 # checker.draw()
 # checker.show()
+#
+# circle = Circle(12, 3, [3,4])
+# circle.draw()
+# circle.show()
 
-circle = Circle(12, 3, [3,4])
-circle.draw()
-circle.show()
+spec = Spectrum(32)
+spec.draw()
+spec.show()
 
--- ex01/src_to_implement/pattern.py
+++ ex01/src_to_implement/pattern.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 # might be useful: np.tile(), np.arange(), np.zeros(), np.ones(), np.concatenate() and np.expand dims()
 
 
@@ -38,9 +39,30 @@ class Circle:
 
     def draw(self):
         self.output = np.zeros((self.resolution, self.resolution))
-        y, x = np.ogrid[-self.radius: self.radius, -self.radius: self.radius]
+        x, y = np.ogrid[-self.radius: self.radius, -self.radius: self.radius]
         index = x ** 2 + y ** 2 <= self.radius ** 2
         self.output[self.y_pos - self.radius:self.y_pos + self.radius, self.x_pos - self.radius:self.x_pos + self.radius][index] = 1
+        return np.copy(self.output)
+
+    def show(self):
+        plt.imshow(self.output)
+        plt.show()
+
+
+class Spectrum:
+    def __init__(self, resolution):
+        self.resolution = resolution
+        self.output = None
+
+    def draw(self):
+        R = np.arange(0.0, 1.0, 1 / self.resolution)
+        G = np.transpose(np.atleast_2d(np.arange(0.0, 1.0, 1 / self.resolution)))
+        B = np.flip(np.arange(0.0, 1.0, 1 / self.resolution))
+
+        self.output = np.zeros((self.resolution, self.resolution, 3))
+        self.output[..., 0] = R
+        self.output[..., 1] = G
+        self.output[..., 2] = B
         return np.copy(self.output)
 
     def show(self):
